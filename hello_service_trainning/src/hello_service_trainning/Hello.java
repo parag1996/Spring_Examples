d75b859bfc89bc840bca8a15558f070d3bd6e6a2
PP5050255 <PP5050255@SYNPSZD4867.syntelorg.com>
2019-02-22 11:10:29 +0530

    Change-Id: I37ff1e66ce309b321ae062df796ef93b34ce0bb7

---
+++ hello_service_trainning/src/hello_service_trainning/Hello.java
@@ -0,0 +1,15 @@
+package hello_service_trainning;
+
+public class Hello {
+
+	void get()
+	{
+		System.out.println();
+	}
+	public static void main(String args[])
+	{
+		Hello hello=new Hello();
+		
+		System.out.println("Hello Spring Boot");
+	}
+}
